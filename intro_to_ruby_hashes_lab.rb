40712fc1d7c20865e23ab8b3c7445abf1ac9b66e
Tom Sharkey <tbsharkey@gmail.com>
2020-03-08 20:57:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly
 	monopoly = {railroads: {}
-}
+	}
 end
 
 def monopoly_with_second_tier
@@ -36,7 +36,7 @@ def monopoly_with_third_tier
 			two_pieces_owned: 50,
 			three_pieces_owned: 100,
 			four_pieces_owned: 200
-		}
+			}
 		}
 	}
 end
